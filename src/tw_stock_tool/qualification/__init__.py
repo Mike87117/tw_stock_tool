c756fbf227c900f5fbd6a33497dc17382b217a30
Mike87117 <52202359+Mike87117@users.noreply.github.com>
2026-08-02 22:19:22 +0800

    Add strategy qualification package exports

---
+++ src/tw_stock_tool/qualification/__init__.py
@@ -0,0 +1,69 @@
+"""Research-only strategy qualification models and pure evaluation boundary."""
+
+from tw_stock_tool.qualification.evaluator import evaluate_strategy_qualification
+from tw_stock_tool.qualification.findings import (
+    SUPPORTED_FINDING_CODES,
+    QualificationFindingError,
+    finding_reason_codes,
+    normalize_findings,
+)
+from tw_stock_tool.qualification.models import (
+    STRATEGY_QUALIFICATION_ARTIFACT_TYPE,
+    STRATEGY_QUALIFICATION_SCHEMA_VERSION,
+    EvidenceScope,
+    FindingSeverity,
+    PromotionState,
+    PromotionDecision,
+    QualificationFinding,
+    QualificationMetricSet,
+    QualificationModelError,
+    QualificationPolicy,
+    StrategyDescriptor,
+    StrategyQualificationRequest,
+    StrategyQualificationResult,
+)
+from tw_stock_tool.qualification.policies import (
+    SUPPORTED_QUALIFICATION_POLICIES,
+    TAIWAN_EQUITY_DAILY_V1,
+    QualificationPolicyError,
+    is_supported_qualification_policy,
+    resolve_qualification_policy,
+)
+from tw_stock_tool.qualification.serialization import (
+    QualificationSerializationError,
+    deserialize_strategy_qualification_result,
+    export_strategy_qualification_json,
+    load_strategy_qualification_json,
+    serialize_strategy_qualification_result,
+)
+
+__all__ = [
+    "STRATEGY_QUALIFICATION_ARTIFACT_TYPE",
+    "STRATEGY_QUALIFICATION_SCHEMA_VERSION",
+    "SUPPORTED_FINDING_CODES",
+    "SUPPORTED_QUALIFICATION_POLICIES",
+    "TAIWAN_EQUITY_DAILY_V1",
+    "EvidenceScope",
+    "FindingSeverity",
+    "PromotionState",
+    "PromotionDecision",
+    "QualificationFinding",
+    "QualificationFindingError",
+    "QualificationMetricSet",
+    "QualificationModelError",
+    "QualificationPolicy",
+    "QualificationPolicyError",
+    "QualificationSerializationError",
+    "StrategyDescriptor",
+    "StrategyQualificationRequest",
+    "StrategyQualificationResult",
+    "deserialize_strategy_qualification_result",
+    "evaluate_strategy_qualification",
+    "export_strategy_qualification_json",
+    "finding_reason_codes",
+    "is_supported_qualification_policy",
+    "load_strategy_qualification_json",
+    "normalize_findings",
+    "resolve_qualification_policy",
+    "serialize_strategy_qualification_result",
+]
